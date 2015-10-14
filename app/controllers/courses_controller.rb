73c5eb7ea73b64acd396234b82ea487758947029
Dmitry BRANAVITSKI <dima_nado@mail.ru>
2015-10-14 23:33:38 +0300

    add action all_courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except:[:all_courses]
 
   def index
     @courses=current_user.courses
@@ -13,6 +13,11 @@ class CoursesController < ApplicationController
     else
       render json: {error: 'Course not create'}, status: 400
     end
+  end
+
+  def all_courses
+    @courses=Course.all
+    render json: @courses, each_serializer: CourseSerializer
   end
 
 end
