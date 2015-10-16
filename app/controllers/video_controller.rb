9dc8ccfcb979164d040eb55ed97f4132f3d9a3b5
Dima B <dima_nado@mail.ru>
2015-10-16 17:40:15 +0300

    fix bug

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,24 +1,40 @@
 class VideoController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_course, only: [:index, :create]
+  before_action :set_video, only: [:show]
 
   def index
-    @videos=Course.find(params[:id_course]).videos
+    @videos = @course.videos
     render json: @videos, each_serializer: VideoNameSerializer
   end
 
   def create
-    video=Course.find(params[:id_course]).videos.new(video: params[:file].tempfile,
-                                                     format: params[:file].content_type,
-                                                     name:params[:name])
+    # binding.pry
+    video = @course.videos.new(video_params)
     if video.save
       render_success
     else
-      render json: {error: 'Video not create'}, status: 400
+      render_error_messages(video, 400)
     end
   end
 
   def show
-    @video=Video.find(params[:id])
     render json: @video, serializer: VideoSerializer
+  end
+
+
+
+  private
+
+  def video_params
+    { video: params[:file].tempfile, format: params[:file].content_type, name:params[:name] }
+  end
+
+  def set_course
+    @course = Course.find(params[:id_course])
+  end
+
+  def set_video
+    @video = Video.find(params[:id])
   end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,6 @@
 class Video < ActiveRecord::Base
   belongs_to :course
 
-
   mount_uploader :video, VideoUploader
 
-  def set_success(format, opts)
-    self.success = true
-  end
 end
