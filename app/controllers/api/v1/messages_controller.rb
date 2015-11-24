b0fac9704a626490bec38d3ade9589838a794e07
Maxim Gorokhovich <horokhovich@gmail.com>
2015-11-24 17:06:48 +0300

    add websockets

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,5 @@
 class API::V1::MessagesController < WebsocketRails::BaseController
   def create
-    byebug
     WebsocketRails.users[myUser.id].send_message('new_notification', { text: message[:text] })
   end
 end
