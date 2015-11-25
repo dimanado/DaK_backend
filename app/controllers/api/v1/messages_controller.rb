e4c8faf811fd98841e733631b44c6e1bc8e7c298
Maxim Gorokhovich <horokhovich@gmail.com>
2015-11-25 21:07:55 +0300

    fix websockets

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,5 +1,6 @@
 class API::V1::MessagesController < WebsocketRails::BaseController
   def create
-    WebsocketRails.users[myUser.id].send_message('new_notification', { text: message[:text] })
+    debugger;
+    WebsocketRails.users["chatChannel_#{message[:channel]}"].send_message('message.create', { text: message[:text] })
   end
 end
